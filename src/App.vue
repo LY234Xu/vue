c09423b5ddb90cadc9a76f4989cd65684881f38f
LY234Xu <3311056440@qq.com>
2025-11-09 15:12:31 +0800

    主体布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
+<!-- App.vue文件是项目入口组件、全局容器（相当于 “整个 App 的外壳”） -->
+
 <template>
   <div id="app">
-    
-    <router-view/>
+    <!-- 头部区域 -->
+    <div style="height:60px;line-height: 60px;background-color: white;margin-bottom: 2px;">
+      <img src="@/assets/logo.png" alt="网站logo" style="width:50px;position: relative;top:5px;left:10px">
+      <!-- position: relative;让元素相对于在父类容器中自身原始位置进行偏移，且不脱离文档流 -->
+      <span style="position: relative;left:20px;bottom:8px;font-size: 34px;">图书管理系统</span>
+    </div>
+
+    <!-- 侧边栏和主体 -->
+    <div style="display: flex;">
+      <!-- 侧边栏导航） -->
+      <div style="width: 200px;min-height:calc(100vh - 62px); overflow: hidden; margin-right: 2px;background-color: white;">
+        <el-menu :default-active="$route.path" :default-openeds="['/']" router class="el-menu-demo">
+          <el-menu-item index="/">
+            <i class="el-icon-s-home"></i>
+            <span>首页</span>
+          </el-menu-item>
+          <el-submenu index="/">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="about">
+              <i class="el-icon-info"></i>
+              <span>关于</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+
+      <!-- 主体数据 -->
+      <div style="flex:1;background-color: white;padding:10px">
+        <router-view /> <!-- 展示路由到的界面 -->
+      </div>
+
+    </div>
+
   </div>
 </template>
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,30 @@
+<!-- 具体业务页面（相当于 “首页这个具体页面”） -->
+
+
+<!-- vue要求template标签下只能有一个根元素（如div等） -->
 <template>
   <div>
-    <el-button type="success">按钮</el-button>
+    <!-- 搜索表单 -->
+    <el-input style="width:240px;margin-left: 5px;" placeholder="请输入名称"></el-input>
+    <el-input style="width:240px;margin-left: 5px;" placeholder="请输入联系方式"></el-input>
+    <el-button type="primary" round style="margin-left: 5px;">
+      <i class="el-icon-search">搜索</i>
+    </el-button>
+
+    <el-table :data="tableData" stripe>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column prop="phone" label="联系方式"></el-table-column>
+    </el-table>
+
+
+    <!-- 分页 -->
+    <div style="margin-top: 20px;">
+      <el-pagination background layout="prev, pager, next" :page-size="5" :total="100">
+      </el-pagination>
+    </div>
+
   </div>
 </template>
 
@@ -8,5 +32,17 @@
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      tableData: [
+        { name: '唐三', age: 20, address: '汕头', phone: '18318212567' },
+        { name: '唐三', age: 20, address: '汕头', phone: '18318212567' },
+        { name: '唐三', age: 20, address: '汕头', phone: '18318212567' },
+        { name: '唐三', age: 20, address: '汕头', phone: '18318212567' },
+        { name: '唐三', age: 20, address: '汕头', phone: '18318212567' },
+        { name: '唐三', age: 20, address: '汕头', phone: '18318212567' },
+      ]
+    }
+  }
 }
 </script>
